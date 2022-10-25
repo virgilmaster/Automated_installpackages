80d6652af71b857b65ce36295142b53110269eae
yefengx.he <yefengx.he@intel.com>
2022-10-25 11:23:03 +0800

    Update installation.py
    This is today's morning commit.

--- installation.py
+++ installation.py
@@ -12,8 +12,6 @@ import urllib.error
 
 def main():
     start = time.perf_counter()
-    # This function can read the requirement.txt by readline and to install the correct version of packages
-    # read_requirement =
     packages_install = input('Please enter the Python package you want to install: ')
     print('The Python package you want to install is: ', packages_install)
     check_packages(packages_install)
@@ -27,24 +25,33 @@ def check_packages(packages_install):
     operation_system = platform.system()
     print("Start to check whether the Python package is installed")
     if (operation_system == 'Windows'):
-        command_result = os.system('pip list | findstr ' + packages_install + '> ' + ' checkversion.txt')
+        os.system('pip list | findstr ' + packages_install + '> ' + ' checkversion.txt')
         username = os.getlogin()
         print('Dear guests your operation system is: ' + operation_system + ' The ' + packages_install + ' in you laptop is version:')
 
     elif (operation_system == 'Linux'):
-        command_result = os.system('pip list | grep ' + packages_install + '> ' + ' checkversion.txt')
+        os.system('pip list | grep ' + packages_install + '> ' + ' checkversion.txt')
         username = os.system('whoami')
         print('Dear guests your operation system is: ' + operation_system + ' The ' + packages_install + ' in you laptop is version:')
 
-    # with open('checkversion.txt','w') as file:
-    #     command_result = file.read()
 
-    if command_result == packages_install:
+    # 打开检查功能
+    # This function can read the requirement.txt by readline and to install the correct version of packages
+    # read_requirement =
+    with open('checkversion.txt','r') as file:
+        packages_result = file.read()
+        version_result = (file.read()).split(' ')[-1]
+        print(packages_result.split(' ')[0])
+        print(version_result)
+
+    # 判断安装包是否存在
+    while (packages_result).split(' ')[0] == packages_install:
         print(packages_install + " has been installed on this computer")
         sys.exit()
+        
     else:
         print("Start to setup " + packages_install + " you want to installed in this laptop")
-
+    # 判断安装包的版本信息是否匹配
 
     if os.path.exists("packages_log.txt") == True:
         with open("packages_log.txt", "w") as file:
@@ -63,7 +70,10 @@ def check_packages(packages_install):
 
 
 # Addtional function to add more link pools in the dictionary.
-def install_packages(packages_install):
+# Installing the packages 
+# The first step is to check the version
+# The second step is to install the packages
+def install_packages(packages_install,version_result):
     mirrors_pool = {"pypi.tuna.tsinghua.edu.cn": "https://pypi.tuna.tsinghua.edu.cn/simple",
                     "pypi.douban.com": "http://pypi.douban.com/simple/",
                     "mirrors.aliyun.com": "http://mirrors.aliyun.com/pypi/simple/"}
@@ -75,24 +85,31 @@ def install_packages(packages_install):
 
     # CDing the functions will more powerfule.Use the beautiful Algorithm to make the programe more wonderful.Learn to use dictionary.
     # 2022-10-24: Today's aim is to slove this function.
-    try:
-        pip_install = os.system('pip install ' + packages_install + " -i https://pypi.tuna.tsinghua.edu.cn/simple" + ' --trusted-host ' + " pypi.tuna.tsinghua.edu.cn")
-        response_tsing = urllib.request.urlopen('https://pypi.tuna.tsinghua.edu.cn',timeout=5)
-    except urllib.error.URLError as e:
-        if isinstance(e.reason, socket.timeout):
-            print('tsinghua requested timeout')
-    try:
-        pip_install = os.popen('pip install ' + packages_install + " -i http://pypi.douban.com/simple/" + ' --trusted-host ' + " pypi.douban.com")
-        response_douban = urllib.request.urlopen('http://pypi.douban.com',timeout=20)
-    except urllib.error.URLError as e1:
-        if isinstance(e1.reason, socket.timeout):
-            print('douban requested timeout')
-    try:
-        pip_install = os.popen('pip install ' + packages_install + " -i http://mirrors.aliyun.com/pypi/simple/" + ' --trusted-host ' + " mirrors.aliyun.com")
-        response_aliyun = urllib.request.urlopen('http://mirrors.aliyun.com',timeout=15)
-    except urllib.error.URLError as e2:
-        if isinstance(e2.reason, socket.timeout):
-            print('aliyun requested timeout')
+    # 2022-10-25: Today use while loop to slove this problem 
+    while version_install == version_result:
+        print()
+
+    # logical problems:
+    # try:
+    #     pip_install = os.system('pip install ' + packages_install + " -i https://pypi.tuna.tsinghua.edu.cn/simple" + ' --trusted-host ' + " pypi.tuna.tsinghua.edu.cn")
+    #     response_tsing = urllib.request.urlopen('https://pypi.tuna.tsinghua.edu.cn',timeout=5)
+    # except urllib.error.URLError as e:
+    #     if isinstance(e.reason, socket.timeout):
+    #         print('tsinghua requested timeout')
+    # try:
+    #     pip_install = os.popen('pip install ' + packages_install + " -i http://pypi.douban.com/simple/" + ' --trusted-host ' + " pypi.douban.com")
+    #     response_douban = urllib.request.urlopen('http://pypi.douban.com',timeout=20)
+    # except urllib.error.URLError as e1:
+    #     if isinstance(e1.reason, socket.timeout):
+    #         print('douban requested timeout')
+    # try:
+    #     pip_install = os.popen('pip install ' + packages_install + " -i http://mirrors.aliyun.com/pypi/simple/" + ' --trusted-host ' + " mirrors.aliyun.com")
+    #     response_aliyun = urllib.request.urlopen('http://mirrors.aliyun.com',timeout=15)
+    # except urllib.error.URLError as e2:
+    #     if isinstance(e2.reason, socket.timeout):
+    #         print('aliyun requested timeout')
+    
+    # Function: 安装完之后在把，安装版本的信息写进去
 
 
 def counter_process(runtime):
