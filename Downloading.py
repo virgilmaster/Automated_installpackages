7afb9be13e1be25b9dd1df2cc13921a43d279685
Virgil <84551549+heyefeng@users.noreply.github.com>
2022-10-31 22:14:21 +0800

    Update Downloading.py

--- Downloading.py
+++ Downloading.py
@@ -3,14 +3,80 @@ import time
 import datetime
 import jmespath
 from jmespath import search
-import urllib3
-import requests
-import re
-
-
+from automation_upgrade import read_requirements
+from automation_upgrade import handle_packages
+import platform
 
 # Json data from dictionary
-def installation_mirrors():
+def installation_mirrors(mirror_pools):
+    with open("Mirrors_links.json", "w") as mirrors_file:
+        mirrors_file.write(str(mirror_pools))
+        mirrors_file.close()
+    print("Wait a moment,loading the settings")
+
+def check_packages():
+    operation_system = platform.system()
+    if operation_system == "Windows":
+        pack_num = os.popen('type requirements.txt | find /v /c""')
+        username = os.getlogin()
+        output_num = pack_num.read()
+        pack_num.close()
+    elif operation_system == "Linux":
+        pack_num = os.popen('cat requirements.txt | wc -l"')
+        command_username = os.popen('whoami')
+        username = command_username.read()
+        output_num = pack_num.read()
+        command_username.close()
+        pack_num.close()
+
+    pack_information = read_requirements("requirements.txt")
+    numbers = int(output_num)
+    for i in range(numbers):
+        package_detail = str(pack_information[i])
+        package_result = package_detail.split("'")[1].split("'")[0]
+        converted_result = package_result.split(",")[0]
+        package_names = package_result.split("==")[0]
+        package_version = package_result.split("==")[1]
+        print(converted_result)
+
+    aliyun_data = search("aliyun",mirror_pools)
+    final_ali = str(aliyun_data)
+    aliyun_domain = final_ali.split(' ')[1].replace("'","").replace(",","")
+    aliyun_link = final_ali.split(' ')[3].replace("'","").replace("}","").replace("]","")
+    try:
+        os.system("pip install " + converted_result + "-i" + " " + aliyun_link + " --trusted-host " + aliyun_domain)
+    except Exception as e:
+        print(e)
+
+    tsinghua_data = search("tsinghua",mirror_pools)
+    final_tsinghua = str(tsinghua_data)
+    tsinghua_domain = final_tsinghua.split(' ')[1].replace("'","").replace(",","")
+    tsinghua_link = final_tsinghua.split(' ')[3].replace("'","").replace("}","").replace("]","")
+    try:
+        os.system("pip install " + converted_result + "-i" + " " + tsinghua_link + " --trusted-host " + tsinghua_domain)
+    except Exception as e:
+        print(e)
+
+    douban_data = search("douban",mirror_pools)
+    final_douban = str(douban_data)
+    douban_domain = final_douban.split(' ')[1].replace("'","").replace(",","")
+    douban_link = final_douban.split(' ')[3].replace("'","").replace("}","").replace("]","")
+    try:
+        os.system("pip install " + converted_result + "-i" + " " + douban_link + " --trusted-host " + douban_domain)
+    except Exception as e:
+        print(e)
+
+    ustc_data = search("ustc",mirror_pools)
+    final_ustc = str(ustc_data)
+    ustc_domain = final_ustc.split(' ')[1].replace("'","").replace(",","")
+    ustc_link = final_ustc.split(' ')[3].replace("'","").replace("}","").replace("]","")
+    try:
+        os.system("pip install " + converted_result + "-i" + " " + ustc_link + " --trusted-host " + ustc_domain)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
     mirror_pools = {"aliyun": [
     {"domain": "mirrors.aliyun.com","link":"http://mirrors.aliyun.com/pypi/simple/"}],
     "ustc":[
@@ -20,75 +86,6 @@ def installation_mirrors():
     "tsinghua":[
     {"domain":"pypi.tuna.tsinghua.edu.cn","link": "https://pypi.tuna.tsinghua.edu.cn/simple/"}]
     }
-    print("Start downloading the packages from internet")
-    aliyun_data = search("aliyun",mirror_pools)
-    final_ali = str(aliyun_data)
-    aliyun_domain = final_ali.split(' ')[1].replace("'","").replace(",","")
-    aliyun_link = final_ali.split(' ')[3].replace("'","").replace("}","").replace("]","")
-    
-    ustc_data = search("ustc",mirror_pools)
-    ustc_domain = final_ali.split(' ')[1].replace("'","").replace(",","")
-    ustc_link = final_ali.split(' ')[3].replace("'","").replace("}","").replace("]","")
-    print(ustc_domain)
-    print(ustc_link)
-    douban_data = search("douban",mirror_pools)
-    douban_domain = final_ali.split(' ')[1].replace("'","").replace(",","")
-    douban_link = final_ali.split(' ')[3].replace("'","").replace("}","").replace("]","")
-    print(douban_domain)
-    print(douban_link)
-    tsinghua_data = search("tsinghua",mirror_pools)
-    tsinghua_domain = final_ali.split(' ')[1].replace("'","").replace(",","")
-    tsinghua_link = final_ali.split(' ')[3].replace("'","").replace("}","").replace("]","")
-    print(tsinghua_domain)
-    print(tsinghua_link)
+    check_packages()
+    installation_mirrors(mirror_pools)
 
-
-
-# def aliyun_download():
-#     try:
-#         os.system("pip install" + " " + "-i" + " " + "$links" + "--trusted-host" + "$domain")
-#     except
-# def tsinghua_download():
-#     try:
-#         os.system("pip install" + " " + "-i" + " " + "$links" + "--trusted-host" + "$domain")
-#     except
-# def douban_download():
-#     try:
-#         os.system("pip install" + " " + "-i" + " " + "$links" + "--trusted-host" + "$domain")
-#     except
-# def ustc_download():
-#     try:
-#         os.system("pip install" + " " + "-i" + " " + "$links" + "--trusted-host" + "$domain")
-#     except
-
-
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
- 
-# def boxPrint(symbol, width, height):
-#     if len(symbol) != 1:
-#         raise Exception("Symbol must be a single character string.")
-#     if width <= 2:
-#         raise Exception("Width must be greater than 2.")
-#     if height <= 2:
-#         raise Exception("Height must be greater than 2.")
- 
-#     print(symbol * width)
-#     for i in range(height - 2):
-#         print(symbol + (' ' * (width - 2)) + symbol)
-#     print(symbol * width)
- 
- 
-# for sym, w, h in (('*', 4, 4), ('O', 20, 5), ('x', 1, 3), ('ZZ', 3, 3)):
-#     try:
-#         boxPrint(sym, w, h)
-#     except Exception as err:
-#         print('An exception happened: ' + str(err))
-
-
-if __name__ == '__main__':
-    installation_mirrors()
-    # aliyun_download()
-    # tsinghua_download()
-    # douban_download()
-    # ustc_download()
