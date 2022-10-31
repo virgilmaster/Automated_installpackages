45f0f6a200e65b3f04ed23764c579a332b4b22cf
Virgil <84551549+heyefeng@users.noreply.github.com>
2022-10-31 12:06:25 +0800

    Update automation_upgrade.py

--- automation_upgrade.py
+++ automation_upgrade.py
@@ -1,14 +1,11 @@
-# Author: Virgil
+# Author: Virgil.babala
 # Functions: To download the python packages from internet automactically
 # Email-address: 691267837@qq.com
-# Date: 2022/10/28
-# Company: Intel
-# Version: 0.0.3
+# Date: 2022/10/31
+# Version: 0.0.5
+# Fundation: Virgil@copyright.org
 
 import os
-from string import punctuation
-import string
-from struct import pack
 import re,string
 import time
 import datetime
@@ -19,11 +16,8 @@ from unittest import result
 import urllib.request
 import urllib.error
 import requests
-import logging
 
 
-
-    
 
 def read_requirements(file_name):
     pack_information = []
@@ -57,9 +51,10 @@ def handle_packages(output_num,pack_information):
         package_result = package_detail.split("'")[1].split("'")[0]
         package_names = package_result.split("==")[0]  # The package's names
         package_version = package_result.split("==")[1] # The packages's version
-        final_version = re.sub('[%s]' % re.escape(string.punctuation), '', package_version)
-        print(final_version)
+        final_version = re.sub('[%s]' % re.escape(string.punctuation), '', package_version) # The converted result guests want
         
+        
+        # Not wonderful function have some Bugs
         if operation_system == "Windows":
             packages_installed = os.popen("pip list | findstr " + package_names)
             result_installed = packages_installed.read()
@@ -76,9 +71,30 @@ def handle_packages(output_num,pack_information):
             packages_installed = os.popen("pip list | grep " + package_names)
             result_installed = packages_installed.read()
             packages_installed.close()
+            installed_version = result_installed.split(" ")[-1]
+            final_installed = re.sub('[%s]' % re.escape(string.punctuation), '', installed_version)
+            print(final_installed)
+            while final_installed == final_version:
+                print("Your " + package_names + " 's version is correct have no necessary to install")
+                
+            else:
+                print("Your " + package_names + " 's version is not correct")
+                
 
+def installation_packages():
+    # The mirror resources can handle the problem downloading from internet
+    mirror_pools = {"pypi.tuna.tsinghua.edu.cn": "https://pypi.tuna.tsinghua.edu.cn/simple",
+                    "pypi.douban.com": "http://pypi.douban.com/simple/",
+                    "mirrors.aliyun.com": "http://mirrors.aliyun.com/pypi/simple/",
+                    "pypi.mirrors.ustc.edu.cn":"https://pypi.mirrors.ustc.edu.cn/simple/"}
 
+    with open("mirrors_links.json","w") as mirrors_file:
+        mirrors_file.write(str(mirror_pools))
+        mirrors_file.close()
     
+    
+
+
 if __name__ == "__main__":
     #start_counter = time.perf_counter()
     file_name = 'requirements.txt'
@@ -95,21 +111,12 @@ if __name__ == "__main__":
         output_num = pack_num.read()
         pack_num.close()
     handle_packages(output_num,pack_information)
-    #package_detail = str(pack_information[i])
-    #convert_detail(package_detail)
+    installation_packages()
     # end_counter = time.perf_counter()
     # runtime = end_counter - start_counter
     # counter_process(runtime)
 
 
-    #print(fd)
-    #punctuation = "[',] "
-    # print(re.sub(r"[%s]+" %punctuation, "",step_test))
-    # test_result = re.sub(r"[%s]+ punctuation", " ",step_test)
-    # print(test_result)
-    #print(re.sub(r"[%s]+" str(punctuation), str(pack_information[1])))
-    #print(pack_information[2])
-    
 
 
 
