359ec0b34b173d1ca96628cacae64b92b0ff21d3
Virgil <84551549+heyefeng@users.noreply.github.com>
2022-11-02 10:48:14 +0800

    Update get_download.py
    Using the threading spiders to download the resources from internet.

--- get_download.py
+++ get_download.py
@@ -2,41 +2,49 @@ from jmespath import search
 import requests
 import os
 import threading
+import winproxy
 
-
-def get_result(aliyun_domain,tsinghua_domain,douban_domain,ustc_domain):
+def write_mirrors():
     with open("Mirrors_links.json", "w") as mirrors_file:
         mirrors_file.write(str(mirror_pools))
         mirrors_file.close()
     print("Wait a moment,loading the settings")
 
-
+def ali_spider(aliyun_domain):
     r_ali = requests.get("http://" + aliyun_domain)
-    r_tsinghua = requests.get("http://" + tsinghua_domain)
-    r_douban = requests.get("http://" + douban_domain)
-    r_ustc = requests.get("http://" + ustc_domain)
-
     code_ali = r_ali.status_code
-    code_tsinghua = r_tsinghua.status_code
-    code_douban = r_douban.status_code
-    code_ustc = r_ustc.status_code
-
-
     if code_ali != 200:
-        raise Exception("aliyun can not download the resources")
-    elif code_tsinghua != 200:
-        raise Exception("tsinghua can not download the resources")
-    elif code_douban != 200:
-        raise Exception("douban can not download the resources")
-    elif code_ustc != 200:
-        raise Exception("ustc can not download the resources")
+        print("ali's requests is error")
+        #raise Exception("aliyun can not download the resources")
+    else:
+        print("Perpare to download the resources!!!")
 
+def tsinghua_spider(tsinghua_domain):
+    r_tsinghua = requests.get("http://" + tsinghua_domain)
+    code_tsinghua = r_tsinghua.status_code
+    if code_tsinghua != 200:
+        print("tsinhua's requests is error")
+        #raise Exception("tsinghua can not download the resources")
+    else:
+        print("Perpare to download the resources!!!")
 
-def downloading_resource():
-    task_ali = os.system()
-    task_ustc = os.system()
-    task_tsinghua = os.system()
-    task_douban = os.system()
+def ustc_spider(ustc_domain):
+    r_ustc = requests.get("http://" + ustc_domain)
+    code_ustc = r_ustc.status_code
+    if code_ustc != 200:
+        print("ustc's requests is error")
+        #raise Exception("ustc can not download the resources")
+    else:
+        print("Perpare to download the resources!!!")
+
+def douban_spider(douban_domain):
+    r_douban = requests.get("http://" + douban_domain)
+    code_douban = r_douban.status_code
+    if code_douban != 200:
+        print("douban's requests is error")
+        #raise Exception("douban can not download the resources")
+    else:
+        print("Perpare to download the resources!!!")
 
 
 
@@ -54,21 +62,20 @@ if __name__ == '__main__':
     final_ali = str(aliyun_pool)
     aliyun_domain = final_ali.split(' ')[1].replace("'", "").replace(",", "")
     aliyun_link = final_ali.split(' ')[3].replace("'", "").replace("}", "").replace("]", "")
-
     tsinghua_pool = search("tsinghua", mirror_pools)
     final_tsinghua = str(tsinghua_pool)
     tsinghua_domain = final_tsinghua.split(' ')[1].replace("'", "").replace(",", "")
     tsinghua_link = final_tsinghua.split(' ')[3].replace("'", "").replace("}", "").replace("]", "")
-
     douban_pool = search("douban", mirror_pools)
     final_douban = str(douban_pool)
     douban_domain = final_douban.split(' ')[1].replace("'", "").replace(",", "")
     douban_link = final_douban.split(' ')[3].replace("'", "").replace("}", "").replace("]", "")
-
     ustc_pool = search("ustc", mirror_pools)
     final_ustc = str(ustc_pool)
     ustc_domain = final_ustc.split(' ')[1].replace("'", "").replace(",", "")
     ustc_link = final_ustc.split(' ')[3].replace("'", "").replace("}", "").replace("]", "")
-
-    get_result(aliyun_domain,tsinghua_domain,douban_domain,ustc_domain)
-    downloading_resource()
+    write_mirrors()
+    task_ali = threading.Thread(target=ali_spider,args=aliyun_domain)
+    task_tsinghua = threading.Thread(target=tsinghua_spider,args=tsinghua_domain)
+    task_ustc = threading.Thread(target=ustc_spider,args=ustc_domain)
+    task_douban = threading.Thread(target=douban_spider,args=douban_domain)
