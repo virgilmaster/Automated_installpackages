95e1fd522abc4af21d32e77bc2a9a30a6397b041
Virgil <84551549+heyefeng@users.noreply.github.com>
2022-11-02 09:37:44 +0800

    Update and rename boxer.py to get_download.py

--- get_download.py
+++ get_download.py
@@ -1,14 +1,55 @@
 from jmespath import search
 import requests
 import os
+import threading
 
 
-def get_result(mirror_pools):
+def get_result(aliyun_domain,tsinghua_domain,douban_domain,ustc_domain):
     with open("Mirrors_links.json", "w") as mirrors_file:
         mirrors_file.write(str(mirror_pools))
         mirrors_file.close()
     print("Wait a moment,loading the settings")
 
+
+    r_ali = requests.get("http://" + aliyun_domain)
+    r_tsinghua = requests.get("http://" + tsinghua_domain)
+    r_douban = requests.get("http://" + douban_domain)
+    r_ustc = requests.get("http://" + ustc_domain)
+
+    code_ali = r_ali.status_code
+    code_tsinghua = r_tsinghua.status_code
+    code_douban = r_douban.status_code
+    code_ustc = r_ustc.status_code
+
+
+    if code_ali != 200:
+        raise Exception("aliyun can not download the resources")
+    elif code_tsinghua != 200:
+        raise Exception("tsinghua can not download the resources")
+    elif code_douban != 200:
+        raise Exception("douban can not download the resources")
+    elif code_ustc != 200:
+        raise Exception("ustc can not download the resources")
+
+
+def downloading_resource():
+    task_ali = os.system()
+    task_ustc = os.system()
+    task_tsinghua = os.system()
+    task_douban = os.system()
+
+
+
+if __name__ == '__main__':
+    mirror_pools = {"aliyun": [
+    {"domain": "mirrors.aliyun.com","link":"http://mirrors.aliyun.com/pypi/simple/"}],
+    "ustc":[
+    {"domain":"pypi.mirrors.ustc.edu.cn","link": "https://pypi.mirrors.ustc.edu.cn/simple/"}],
+    "douban":[
+    {"domain":"pypi.douban.com","link": "http://pypi.douban.com/simple/"}],
+    "tsinghua":[
+    {"domain":"pypi.tuna.tsinghua.edu.cn","link": "https://pypi.tuna.tsinghua.edu.cn/simple/"}]
+    }
     aliyun_pool = search("aliyun", mirror_pools)
     final_ali = str(aliyun_pool)
     aliyun_domain = final_ali.split(' ')[1].replace("'", "").replace(",", "")
@@ -29,42 +70,5 @@ def get_result(mirror_pools):
     ustc_domain = final_ustc.split(' ')[1].replace("'", "").replace(",", "")
     ustc_link = final_ustc.split(' ')[3].replace("'", "").replace("}", "").replace("]", "")
 
-
-    r_ali = requests.get("http://" + aliyun_domain)
-    r_tsinghua = requests.get("http://" + tsinghua_domain)
-    r_douban = requests.get("http://" + douban_domain)
-    r_ustc = requests.get("http://" + ustc_domain)
-
-    code_ali = r_ali.status_code
-    code_tsinghua = r_tsinghua.status_code
-    code_douban = r_douban.status_code
-    code_ustc = r_ustc.status_code
-
-    print(code_ali)
-    print(code_tsinghua)
-    print(code_douban)
-    print(code_ustc)
-
-    if code_ali != 200:
-        raise Exception("aliyun can not download the resources")
-    elif code_tsinghua != 200:
-        raise Exception("tsinghua can not download the resources")
-    elif code_douban != 200:
-        raise Exception("douban can not download the resources")
-    elif code_ustc != 200:
-        raise Exception("ustc can not download the resources")
-
-    try:
-        os
-
-if __name__ == '__main__':
-    mirror_pools = {"aliyun": [
-    {"domain": "mirrors.aliyun.com","link":"http://mirrors.aliyun.com/pypi/simple/"}],
-    "ustc":[
-    {"domain":"pypi.mirrors.ustc.edu.cn","link": "https://pypi.mirrors.ustc.edu.cn/simple/"}],
-    "douban":[
-    {"domain":"pypi.douban.com","link": "http://pypi.douban.com/simple/"}],
-    "tsinghua":[
-    {"domain":"pypi.tuna.tsinghua.edu.cn","link": "https://pypi.tuna.tsinghua.edu.cn/simple/"}]
-    }
-    get_result(mirror_pools)
+    get_result(aliyun_domain,tsinghua_domain,douban_domain,ustc_domain)
+    downloading_resource()
