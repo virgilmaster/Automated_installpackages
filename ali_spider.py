231dc343f2a134dae68aa740750b5a4e34a707cd
Virgil <84551549+heyefeng@users.noreply.github.com>
2022-11-04 23:42:56 +0800

    Update ali_spider.py

--- ali_spider.py
+++ ali_spider.py
@@ -1,11 +1,9 @@
-import os 
+import os
 import requests
 from jmespath import search
 import time
 import platform
 
-
-# 现在阶段:能跑起来就行 
 
 def read_requirements1(file_name):
     pack_information = []  
@@ -18,45 +16,38 @@ def read_requirements1(file_name):
     return pack_information
 
 
-def handle_packages1(pack_information):
+def ali_downloader(pack_information):
     operation_system = platform.system()
     if operation_system == "Windows":
         pack_num = os.popen('type requirements.txt | find /v /c""')
-        username = os.getlogin()
         output_num = pack_num.readlines()
         final_num = str(output_num[0]).replace("\n",'')
         pack_num.close()
     elif operation_system == "Linux":
         pack_num = os.popen('cat requirements.txt | wc -l"')
         command_username = os.popen('whoami')
-        username = command_username.read()
         output_num = pack_num.readlines()
         final_num = str(output_num[0]).replace("\n",'')
         command_username.close()
         pack_num.close()
     numbers = int(final_num) 
-    for i in range(numbers):  
+    for i in range(numbers): 
         package_detail = str(pack_information[i]) 
         package_result = package_detail.split("'")[1].split("'")[0]
-        package_names = package_result.split("==")[0]
-    return package_names
-
-
-def ali_downloader(package_names):
-    aliyun_pool = search("aliyun", mirror_pools)
-    final_ali = str(aliyun_pool)
-    aliyun_domain = final_ali.split(' ')[1].replace("'", "").replace(",", "")
-    aliyun_link = final_ali.split(' ')[3].replace("'", "").replace("}", "").replace("]", "")
-    r_ali = requests.get("http://" + aliyun_domain)
-    code_ali = r_ali.status_code
-    if code_ali != 200:
-        print("ali's requests is error")
-        raise Exception("aliyun can not download the resources")
-    else:
-        print("Perpare to download the resources!!!")
-        time.sleep(5)
-        os.system("pip install " + package_names + " -i " + aliyun_link + " --trusted-host " + aliyun_domain)
-        print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+        aliyun_pool = search("aliyun", mirror_pools)
+        final_ali = str(aliyun_pool)
+        aliyun_domain = final_ali.split(' ')[1].replace("'", "").replace(",", "")
+        aliyun_link = final_ali.split(' ')[3].replace("'", "").replace("}", "").replace("]", "")
+        r_ali = requests.get("http://" + aliyun_domain)
+        code_ali = r_ali.status_code
+        if code_ali != 200:
+            print("ali's requests is error")
+            raise Exception("aliyun can not download the resources")
+        else:
+            print("Perpare to download the resources!!!")
+            time.sleep(5)
+            os.system("pip install " + package_result.replace(",","") + " -i " + aliyun_link + " --trusted-host " + aliyun_domain)
+            print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 
 
 if __name__ == '__main__':
@@ -72,6 +63,4 @@ if __name__ == '__main__':
     }
     read_requirements1(file_name)
     pack_information = read_requirements1(file_name)
-    handle_packages1(pack_information)
-    package_names = handle_packages1(pack_information)
-    ali_downloader(package_names)
+    ali_downloader(pack_information)
