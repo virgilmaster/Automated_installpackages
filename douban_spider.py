a6dc6a711c5dd86f47a092a11030be0d54f16045
Virgil <84551549+heyefeng@users.noreply.github.com>
2022-11-04 23:44:31 +0800

    Update douban_spider.py

--- douban_spider.py
+++ douban_spider.py
@@ -5,8 +5,6 @@ import time
 import platform
 
 
-# 现在阶段:能跑起来就行 
-
 def read_requirements1(file_name):
     pack_information = []  
     file = open(file_name,'r') 
@@ -18,18 +16,16 @@ def read_requirements1(file_name):
     return pack_information
 
 
-def handle_packages1(pack_information):
+def douban_downloader(pack_information):
     operation_system = platform.system()
     if operation_system == "Windows":
         pack_num = os.popen('type requirements.txt | find /v /c""')
-        username = os.getlogin()
         output_num = pack_num.readlines()
         final_num = str(output_num[0]).replace("\n",'')
         pack_num.close()
     elif operation_system == "Linux":
         pack_num = os.popen('cat requirements.txt | wc -l"')
         command_username = os.popen('whoami')
-        username = command_username.read()
         output_num = pack_num.readlines()
         final_num = str(output_num[0]).replace("\n",'')
         command_username.close()
@@ -38,25 +34,20 @@ def handle_packages1(pack_information):
     for i in range(numbers):  
         package_detail = str(pack_information[i]) 
         package_result = package_detail.split("'")[1].split("'")[0]
-        package_names = package_result.split("==")[0]
-    return package_names
-
-
-def douban_downloader(package_names):
-    douban_pool = search("douban", mirror_pools)
-    final_douban = str(douban_pool)
-    douban_domain = final_douban.split(' ')[1].replace("'", "").replace(",", "")
-    douban_link = final_douban.split(' ')[3].replace("'", "").replace("}", "").replace("]", "")
-    r_ali = requests.get("http://" + douban_domain)
-    code_ali = r_ali.status_code
-    if code_ali != 200:
-        print("ali's requests is error")
-        raise Exception("aliyun can not download the resources")
-    else:
-        print("Perpare to download the resources!!!")
-        time.sleep(5)
-        os.system("pip install " + package_names + " -i " + douban_link + " --trusted-host " + douban_domain)
-        print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+        douban_pool = search("douban", mirror_pools)
+        final_douban = str(douban_pool)
+        douban_domain = final_douban.split(' ')[1].replace("'", "").replace(",", "")
+        douban_link = final_douban.split(' ')[3].replace("'", "").replace("}", "").replace("]", "")
+        r_douban = requests.get("http://" + douban_domain)
+        code_douban = r_douban.status_code
+        if code_douban != 200:
+            print("ali's requests is error")
+            raise Exception("douban can not download the resources")
+        else:
+            print("Perpare to download the resources!!!")
+            time.sleep(5)
+            os.system("pip install " + package_result.replace(",","") + " -i " + douban_link + " --trusted-host " + douban_domain)
+            print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 
 
 if __name__ == '__main__':
@@ -72,6 +63,4 @@ if __name__ == '__main__':
     }
     read_requirements1(file_name)
     pack_information = read_requirements1(file_name)
-    handle_packages1(pack_information)
-    package_names = handle_packages1(pack_information)
-    douban_downloader(package_names)
+    douban_downloader(pack_information)
