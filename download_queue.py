fdb81e001a82f5cd85dbb0c3b22af846d65840b8
Virgil <84551549+heyefeng@users.noreply.github.com>
2022-11-05 12:10:00 +0800

    Update download_queue.py

--- download_queue.py
+++ download_queue.py
@@ -7,18 +7,6 @@ import threading
 from queue import Queue
 
 
-# def alitask():
-#     os.system('python' + " " + 'ali_spider.py')
-
-# def doubantask():
-#     os.system('python' + " " + 'douban_spider.py')
-
-# def tsinghuatask():
-#     os.system('python' + " " + 'tsinghua_spider.py')
-
-# def ustctask():
-#     os.system('python' + " " + 'ustc_spider.py')
-
 
 def installation_packages(x):
     try:
@@ -36,16 +24,36 @@ def installation_packages(x):
 if __name__ == '__main__':
     tasklist = []
     lock = Lock()
-    #choose_task = int(input('Dear master please choose which grabber you want to use: ' + 'Only pick the number: 1.ali 2.douban 3.tsinghua 4.ustc' + '\n'))
-    thread1 = threading.Thread(target=installation_packages, args=('ali_spider.py',))
-    thread2 = threading.Thread(target=installation_packages, args=('douban_spider.py',))
-    thread3 = threading.Thread(target=installation_packages, args=('tsinghua_spider.py',))
-    thread4 = threading.Thread(target=installation_packages, args=('ustc_spider.py',))
-    thread1.start()
-    time.sleep(5)
-    thread2.start()
-    time.sleep(5)
-    thread3.start()
-    time.sleep(5)
-    thread4.start()
-    time.sleep(5)
+    begin_time = time.time()
+    while True:
+        choose_task = int(input('Dear master please choose which grabber you want to use: ' + 'Only pick the number: 1.ali 2.douban 3.tsinghua 4.ustc' + '\n'))
+        if choose_task == 1:
+            thread1 = threading.Thread(target=installation_packages, args=('ali_spider.py',))
+            thread1.start()
+            end_time  = time.time()
+            print("The total time is: %s" % (end_time - begin_time))
+        elif choose_task == 2:
+            thread2 = threading.Thread(target=installation_packages, args=('douban_spider.py',))
+            thread2.start()
+            end_time  = time.time()
+            print("The total time is: %s" % (end_time - begin_time))
+        elif choose_task == 3:
+            thread3 = threading.Thread(target=installation_packages, args=('tsinghua_spider.py',))
+            thread3.start()
+            end_time  = time.time()
+            print("The total time is: %s" % (end_time - begin_time))
+        elif choose_task == 4:
+            thread4 = threading.Thread(target=installation_packages, args=('ustc_spider.py',))
+            thread4.start()
+            end_time  = time.time()
+            print("The total time is: %s" % (end_time - begin_time))
+        elif choose_task != 1:
+            print('Type error,please try again!!!')
+        elif choose_task != 2:
+            print('Type error,please try again!!!')
+        elif choose_task != 3:
+            print('Type error,please try again!!!')
+        elif choose_task != 4:
+            print('Type error,please try again!!!')
+        else:
+            print('Type error,please try again!!!')
