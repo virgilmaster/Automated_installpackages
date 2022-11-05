1ae2a1cb0d74ea065faf9a2bd8a35335758e43ae
Virgil <84551549+heyefeng@users.noreply.github.com>
2022-11-05 11:39:02 +0800

    Update download_queue.py

--- download_queue.py
+++ download_queue.py
@@ -1,66 +1,28 @@
 import os
 import time
-import datetime
 from main import read_requirements
 from main import handle_packages
-import platform
 from multiprocessing import Process,Lock
 import threading
+from queue import Queue
 
 
+# def alitask():
+#     os.system('python' + " " + 'ali_spider.py')
 
-def current_packages():
-    operation_system = platform.system()
-    if operation_system == "Windows":
-        pack_num = os.popen('type requirements.txt | find /v /c""')
-        output_num = pack_num.read()
-        pack_num.close()
+# def doubantask():
+#     os.system('python' + " " + 'douban_spider.py')
 
-    elif operation_system == "Linux":
-        pack_num = os.popen('cat requirements.txt | wc -l"')
-        command_username = os.popen('whoami')
-        output_num = pack_num.read()
-        command_username.close()
-        pack_num.close()
+# def tsinghuatask():
+#     os.system('python' + " " + 'tsinghua_spider.py')
 
-    pack_information = read_requirements("requirements.txt")
-    numbers = int((output_num[0].replace("\n","")))
+# def ustctask():
+#     os.system('python' + " " + 'ustc_spider.py')
 
-    for i in range(numbers):
-        package_detail = str(pack_information[i])
-        package_result = package_detail.split("'")[1].split("'")[0]
 
-def alitask():
-    os.system('python' + " " + 'ali_spider.py')
-
-def doubantask():
-    os.system('python' + " " + 'douban_spider.py')
-
-def tsinghuatask():
-    os.system('python' + " " + 'tsinghua_spider.py')
-
-def ustctask():
-    os.system('python' + " " + 'ustc_spider.py')
-
-# def installation_packages(choose_task):
-#     try:
-#         if choose_task == "1":
-#             alitask()
-#         elif choose_task == "2":
-#             doubantask()
-#         elif choose_task == "3":
-#             tsinghuatask()
-#         elif choose_task == "4":
-#             ustctask()
-
-        #final_task = choose_task
-
-def installation_packages():
+def installation_packages(x):
     try:
-        alitask()
-        doubantask()
-        tsinghuatask()
-        ustctask()
+        os.system('python' + " " + x)
 
     except Exception as err:
         print(err)
@@ -75,12 +37,15 @@ if __name__ == '__main__':
     tasklist = []
     lock = Lock()
     #choose_task = int(input('Dear master please choose which grabber you want to use: ' + 'Only pick the number: 1.ali 2.douban 3.tsinghua 4.ustc' + '\n'))
-    # current_packages()
-    installation_packages()
-    # logical desgign
-    # when spider raise expection
-    # jump to the next spider thread
-    # thread1 = 
-    # thread2 =
-    # thread3 =
-    # thread4 =
+    thread1 = threading.Thread(target=installation_packages, args=('ali_spider.py',))
+    thread2 = threading.Thread(target=installation_packages, args=('douban_spider.py',))
+    thread3 = threading.Thread(target=installation_packages, args=('tsinghua_spider.py',))
+    thread4 = threading.Thread(target=installation_packages, args=('ustc_spider.py',))
+    thread1.start()
+    time.sleep(5)
+    thread2.start()
+    time.sleep(5)
+    thread3.start()
+    time.sleep(5)
+    thread4.start()
+    time.sleep(5)
