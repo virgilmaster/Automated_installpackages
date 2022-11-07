48b80c871264d29e444ac160d1b56acc0cc03926
Virgil <84551549+heyefeng@users.noreply.github.com>
2022-11-07 16:13:34 +0800

    Update log_writer.py

--- log_writer.py
+++ log_writer.py
@@ -23,32 +23,52 @@ def log_record(operation_system,pack_information):
     if operation_system == "Windows":
         username = os.getlogin()
         path_result = os.getcwd()
+        pack_num = os.popen('type requirements.txt | find /v /c""')
+        output_num = pack_num.readlines()
+        final_num = str(output_num[0]).replace("\n",'')
+        pack_num.close()
         c_t = str(datetime.datetime.now().strftime('%Y%m%d'))
         download_dir = path_result + '\downloadlog'
         log_path = download_dir + "download_" + c_t + ".log"
+
     elif operation_system == "Linux":
         path_result = os.getcwd()
         command_username = os.popen('whoami')
         username = command_username.read()
+        pack_num = os.popen('cat requirements.txt | wc -l"')
+        output_num = pack_num.readlines()
+        final_num = str(output_num[0]).replace("\n",'')
+        pack_num.close()
         c_t = str(datetime.datetime.now().strftime('%Y%m%d'))
         download_dir = path_result + '/downloadlog'
         log_path = download_dir + "download_" + c_t + ".log"
 
+    # Althought this function is very complex,but finally i have finished.I feel very excited.Hard to say it by words.All the things i done,it's worth to do.
     if not os.path.exists(download_dir):
         os.mkdir(download_dir)
         if not os.path.exists(log_path):
             os.chdir(download_dir)
-            print(pack_information)
-            current_time = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-            log_builder = logging.getLogger()
-            log_builder.setLevel(logging.INFO)
-            file_hand = logging.FileHandler(filename="download_" + c_t + ".log",mode='a',encoding='utf-8')
-            #stream_hand = logging.StreamHandler()
-            log_format = logging.Formatter("[%(asctime)s]-[%(levelname)s]-[%(pathname)s]-[Line:%(lineno)d]-[LoggerInfo:%(message)s]")
-            #log_format(fmt=log_format)
-            log_builder.addHandler(file_hand)
-            log_builder.info((str(username) + " try to downloading the " + str(pack_information) + " at " + current_time))
             
+
+            numbers = int(final_num)
+            j = 0
+            while j < numbers-1: 
+            #for i in range(numbers):  
+                package_detail = str(pack_information[j]) 
+                package_result = package_detail.split("'")[1].split("'")[0]
+                package_names = package_result.split("==")[0]  
+                package_version = package_result.split("==")[1]
+                j += 1
+                pass
+                i = 0
+                while i <= numbers:
+                    current_time = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+                    log_builder = logging.getLogger()
+                    log_builder.setLevel(logging.INFO)
+                    file_hand = logging.FileHandler(filename="download_" + c_t + ".log",mode='a',encoding='utf-8')
+                    log_builder.addHandler(file_hand)
+                    log_builder.info((str(username) + " try to downloading the " + package_names + " and the version is " + package_version + " at " + current_time))
+                    i += numbers
             
     
 
