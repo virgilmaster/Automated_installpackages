2fc1359359e185af427fe4ab1910eaacfe9dc8f1
Virgil <84551549+heyefeng@users.noreply.github.com>
2022-11-07 13:13:43 +0800

    Update download_queue.py
    This addtional function can make the pack install more quickily

--- download_queue.py
+++ download_queue.py
@@ -3,7 +3,7 @@ import time
 from multiprocessing import Process,Lock
 import threading
 from queue import Queue
-
+import logging
 
 
 def installation_packages(x):
@@ -20,20 +20,28 @@ def installation_packages(x):
 
 
 if __name__ == '__main__':
-    tasklist = []
+    tasklist = ['ali_spider.py','douban_spider.py','tsinghua_spider.py','ustc_spider.py']
     lock = Lock()
-    choose_task = int(input('Dear master please choose which grabber you want to use: ' + 'Only pick the number: 1.ali 2.douban 3.tsinghua 4.ustc' + '\n'))
-    if choose_task == 1:
-        thread1 = threading.Thread(target=installation_packages, args=('ali_spider.py',))
-        thread1.start()
-    elif choose_task == 2:
-        thread2 = threading.Thread(target=installation_packages, args=('douban_spider.py',))
-        thread2.start()
-    elif choose_task == 3:
-        thread3 = threading.Thread(target=installation_packages, args=('tsinghua_spider.py',))
-        thread3.start()
-    elif choose_task == 4:
-        thread4 = threading.Thread(target=installation_packages, args=('ustc_spider.py',))
-        thread4.start()
-    else:
-        print('Type error,please try again!!!')
+    j = 0
+    while j < 3:
+        tk = threading.Thread(target=installation_packages, args=(tasklist[j],))
+        tk.setDaemon(True)
+        tk.start()
+        #tk.join()
+        time.sleep(20)
+        j += 1
+    # choose_task = int(input('Dear master please choose which grabber you want to use: ' + 'Only pick the number: 1.ali 2.douban 3.tsinghua 4.ustc' + '\n'))
+    # if choose_task == 1:
+    #     thread1 = threading.Thread(target=installation_packages, args=('ali_spider.py',))
+    #     thread1.start()
+    # elif choose_task == 2:
+    #     thread2 = threading.Thread(target=installation_packages, args=('douban_spider.py',))
+    #     thread2.start()
+    # elif choose_task == 3:
+    #     thread3 = threading.Thread(target=installation_packages, args=('tsinghua_spider.py',))
+    #     thread3.start()
+    # elif choose_task == 4:
+    #     thread4 = threading.Thread(target=installation_packages, args=('ustc_spider.py',))
+    #     thread4.start()
+    # else:
+    #     print('Type error,please try again!!!')
