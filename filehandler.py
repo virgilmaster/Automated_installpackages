9696d5d510e56b4a767ccca736ee531c4c6368a1
Virgil <84551549+heyefeng@users.noreply.github.com>
2022-11-14 10:26:18 +0800

    Update filehandler.py

--- filehandler.py
+++ filehandler.py
@@ -2,25 +2,20 @@ import os
 import platform
 
 
-class files:
+class filesdetails:
     def __init__(self,op,filename):
         self.op = op
         self.filename = filename
-        # self.filepath = filepath
-        # self.filetype = filetype
 
     @property
     def counter(self):
-
         filename = self.filename
         if self.op == platform.system():
             pack_num = os.popen('type' + ' ' + filename + '| find /v /c""')
             output_num = pack_num.readlines()
             final_num = str(output_num[0]).replace("\n", '')
             pack_num.close()
-            print('Oh dear guests your system is: ' + str(platform.system()))
-            print(int(final_num))
-            print(type(final_num))
+            return final_num
 
         elif self.op == platform.system():
             pack_num = os.popen('cat requirements.txt | wc -l"')
@@ -29,10 +24,17 @@ class files:
             final_num = str(output_num[0]).replace("\\n", '').replace('\n', '')
             command_username.close()
             pack_num.close()
-            print(final_num)
+            return final_num
 
-        
+    @property
+    def readlines(self):
+        filename = self.filename
+        pack_information = []  
+        file = open(filename,'r') 
+        file_pack_information = file.readlines()
+        for row in file_pack_information:  
+            tmp_list = row.split(' ')
+            tmp_list[-1] = tmp_list[-1].replace('\n',',')
+            pack_information.append(tmp_list)
+        return pack_information
 
-if __name__ == '__main__':
-    file1 = files("Windows","requirements.txt")
-    print(file1.counter)
