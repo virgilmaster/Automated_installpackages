41ea096445c96d65994b211f388ac65dcc23cd05
Virgil <84551549+heyefeng@users.noreply.github.com>
2022-11-14 11:06:30 +0800

    Create collector.py

---
+++ collector.py
@@ -0,0 +1,20 @@
+from jmespath import search
+
+class mirrors:
+    def __init__(self,name):
+        self.name = name
+
+    @property
+    def mirrorspools(self):
+        mirrors_pools = {"aliyun": [
+    {"domain": "mirrors.aliyun.com","link":"http://mirrors.aliyun.com/pypi/simple/"}],
+    "ustc":[
+    {"domain":"pypi.mirrors.ustc.edu.cn","link": "https://pypi.mirrors.ustc.edu.cn/simple/"}],
+    "douban":[
+    {"domain":"pypi.douban.com","link": "http://pypi.douban.com/simple/"}],
+    "tsinghua":[
+    {"domain":"pypi.tuna.tsinghua.edu.cn","link": "https://pypi.tuna.tsinghua.edu.cn/simple/"}]
+    }
+        result = search(self.name,mirrors_pools)
+        return result
+
