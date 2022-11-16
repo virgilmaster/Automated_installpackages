a734f482e0d39bd0090250e0a8decca449c4382d
Virgil <84551549+virgilmaster@users.noreply.github.com>
2022-11-16 16:48:31 +0800

    Update filehandler.py

--- filehandler.py
+++ filehandler.py
@@ -1,7 +1,3 @@
-import os
-import platform
-
-
 class filesdetails:
     def __init__(self,op,filename):
         self.op = op
@@ -9,6 +5,7 @@ class filesdetails:
 
     @property
     def counter(self):
+        import os
         filename = self.filename
         if self.op == 'Windows':
             pack_num = os.popen('type' + ' ' + filename + '| find /v /c""')
@@ -33,4 +30,35 @@ class filesdetails:
             tmp_list[-1] = tmp_list[-1].replace('\n',',')
             pack_information.append(tmp_list)
         return pack_information
+
+    @property
+    def uninstallpack(self):
+        try:
+            import platform
+            from filehandler import filesdetails
+
+        except ImportError as e:
+            print(e)
+
+        os_result = platform.system()
+        result_pack = filesdetails(str(os_result),'requirements.txt')
+        final_num = result_pack.counter
+        pack_info = result_pack.readinfo
+        numbers = int(final_num)
+        
+        try:
+            import re,string
+        except ImportWarning as e:
+            print('Dear guests your python lib have not re and string module,plz download from internet')
+        for i in range(numbers):  
+            package_detail = str(pack_info[i]) 
+            package_result = package_detail.split("'")[1].split("'")[0]
+            package_names = package_result.split("==")[0]  
+            package_version = package_result.split("==")[1]
+            final_version = re.sub('[%s]' % re.escape(string.punctuation), '', package_version)
+        # Self testing functions
+            
+
+
+
 
