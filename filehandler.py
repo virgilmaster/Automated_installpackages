1ca899f2cc4865d03e78cdbb85b27224a2a8c89a
Virgil <84551549+virgilmaster@users.noreply.github.com>
2022-11-17 12:48:26 +0800

    Update filehandler.py

--- filehandler.py
+++ filehandler.py
@@ -32,7 +32,7 @@ class filesdetails:
         return pack_information
 
     @property
-    def uninstallpack(self):
+    def versionfilter(self):
         try:
             import platform
             from filehandler import filesdetails
@@ -50,66 +50,54 @@ class filesdetails:
             import re,string
         except ImportWarning as e:
             print('Dear guests your python lib have not re and string module,plz download from internet')
-        for i in range(numbers):  
-            package_detail = str(pack_info[i]) 
+
+        j = 0
+        while j < numbers:
+        #for i in range(numbers):  
+            package_detail = str(pack_info[j]) 
             package_result = package_detail.split("'")[1].split("'")[0]
             package_names = package_result.split("==")[0]  
             package_version = package_result.split("==")[1]
             final_version = re.sub('[%s]' % re.escape(string.punctuation), '', package_version)
-        # Self testing functions
-        # ======================================================
+            #print(package_names)
+            #print(final_version)
+            j += 1
+        return final_version
+
+
+    @property
+    def namefilter(self):
         try:
-            import os
+            import platform
+            from filehandler import filesdetails
+
         except ImportError as e:
             print(e)
-        if os_result == "Windows":
-            packages_installed = os.popen("pip list | findstr " + package_names) 
-            result_installed = packages_installed.readlines()     
-            packages_installed.close()                              
-            installed_version = str(result_installed).split(" ")[-1]
-            final_installed = (re.sub('[%s]' % re.escape(string.punctuation), '', installed_version)).replace("n","")
-            
 
-            
-            #HashMap = {}
-            uninstall_list = []
-            if installed_version == '[]':
-                print(package_names +  " have not be installed")
-                print("Start launch the webspiders,to download the new " + package_names)
-                uninstall_list.append(package_names)
-
-  
-            elif final_installed == final_version:
-                print("The " + package_names + " is in the same version,no necessary to install " + package_names + " again")
-
-            else:
-                print("The packages's version is " + installed_version)
-                print("Start to change " + package_names + "'s version,plz wait a moment~.~")
-                uninstall_list.append(package_names)
-               
-            print(uninstall_list)    
-            
-
-        elif os_result == "Linux":
-            packages_installed = os.popen("pip list | grep " + package_names) 
-            result_installed = packages_installed.readlines()     
-            packages_installed.close()
-            installed_version = str(result_installed).split(" ")[-1]
-            final_installed = (re.sub('[%s]' % re.escape(string.punctuation), '', installed_version)).replace("n","")
-    
-
+        os_result = platform.system()
+        result_pack = filesdetails(str(os_result),'requirements.txt')
+        final_num = result_pack.counter
+        pack_info = result_pack.readinfo
+        numbers = int(final_num)
         
-            if installed_version == '[]':
-                print(package_names +  " have not be installed")
-                print("Start launch the webspiders,to download the new " + package_names)
-                
-            elif final_installed == final_version:
-                print("The " + package_names + " is in the same version,no necessary to install " + package_names + " again")
-            else:
-                print("The packages's version is " + installed_version)
-                print("Start to change" + package_names + "'s version,plz wait a moment~.~")
-              
-            return uninstall_list
+        try:
+            import re,string
+        except ImportWarning as e:
+            print('Dear guests your python lib have not re and string module,plz download from internet')
+
+        j = 0
+        while j < numbers:
+        #for i in range(numbers):  
+            package_detail = str(pack_info[j]) 
+            package_result = package_detail.split("'")[1].split("'")[0]
+            package_names = package_result.split("==")[0]  
+            package_version = package_result.split("==")[1]
+            final_version = re.sub('[%s]' % re.escape(string.punctuation), '', package_version)
+            #print(package_names)
+            #print(final_version)
+            j += 1
+        return package_names
+
 
 
 
