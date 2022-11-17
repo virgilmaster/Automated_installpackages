740f47f69c885d40f7fe9580a7d0f80ffdb379b0
Virgil <84551549+virgilmaster@users.noreply.github.com>
2022-11-17 14:54:03 +0800

    Update inspector.py

--- inspector.py
+++ inspector.py
@@ -1,5 +1,4 @@
 class checker:
-
     def __init__(self,opsys,filename):
         self.opsys = opsys
         self.filename = filename
@@ -8,58 +7,80 @@ class checker:
     def versioncheck(self):
         try:
             from filehandler import filesdetails
-        except ImportError as e:
-            raise e
+            import os,re,string
+        except ImportError as err:
+            print(err)
+            raise err
 
         os_result = self.opsys
         file_name = self.filename
-        tgfiles = filesdetails(os_result,file_name)
-        # print(tgfiles.filefilter)
 
-        try:
-            import os,re,string
-        except ImportError as e:
-            print(e)
+        fileresult = filesdetails(file_name,os_result)
+        namelist = fileresult.namefilter
+        versionlist = fileresult.versionfilter
+        '''
+        Test function:
+        list of the packages name
+        '''
+        # print(namelist[0])
+        # print(namelist[1])
+        # print(versionlist[0])
+        # print(versionlist[1])
+        """
+        Test pass
+        """
+         
+         
+        # packages_installed = os.popen("pip list | findstr " + package_names) 
+        # result_installed = packages_installed.readlines()     
+        # packages_installed.close()                              
+        # installed_version = str(result_installed).split(" ")[-1]
+        # final_installed = (re.sub('[%s]' % re.escape(string.punctuation), '', installed_version)).replace("n","")
+        # # if os_result == "Windows":
 
-        if os_result == "Windows":
-            packages_installed = os.popen("pip list | findstr " + package_names) 
-            result_installed = packages_installed.readlines()     
-            packages_installed.close()                              
-            installed_version = str(result_installed).split(" ")[-1]
-            final_installed = (re.sub('[%s]' % re.escape(string.punctuation), '', installed_version)).replace("n","")
+
+        # elif os_result == "Linux":       
+
+        # if os_result == "Windows":
+        #     packages_installed = os.popen("pip list | findstr " + package_names) 
+        #     result_installed = packages_installed.readlines()     
+        #     packages_installed.close()                              
+        #     installed_version = str(result_installed).split(" ")[-1]
+        #     final_installed = (re.sub('[%s]' % re.escape(string.punctuation), '', installed_version)).replace("n","")
             
 
             
-            #HashMap = {}
-            uninstall_list = []
-            if installed_version == '[]':
-                uninstall_list.append(package_names) # 需要安装新的包
+        #     #HashMap = {}
+        #     uninstall_list = []
+        #     if installed_version == '[]':
+        #         uninstall_list.append(package_names) # 需要安装新的包
 
 
-            elif final_installed == final_version:
-                print("The " + package_names + " is in the same version,no necessary to install " + package_names + " again")
+        #     elif final_installed == final_version:
+        #         print("The " + package_names + " is in the same version,no necessary to install " + package_names + " again")
 
-            else:
-                uninstall_list.append(package_names) # 需要安装新的包
-            return uninstall_list
+        #     else:
+        #         uninstall_list.append(package_names) # 需要安装新的包
+        #     return uninstall_list
                 
             
 
-        elif os_result == "Linux":
-            packages_installed = os.popen("pip list | grep " + package_names) 
-            result_installed = packages_installed.readlines()     
-            packages_installed.close()
-            installed_version = str(result_installed).split(" ")[-1]
-            final_installed = (re.sub('[%s]' % re.escape(string.punctuation), '', installed_version)).replace("n","")
+        # elif os_result == "Linux":
+        #     packages_installed = os.popen("pip list | grep " + package_names) 
+        #     result_installed = packages_installed.readlines()     
+        #     packages_installed.close()
+        #     installed_version = str(result_installed).split(" ")[-1]
+        #     final_installed = (re.sub('[%s]' % re.escape(string.punctuation), '', installed_version)).replace("n","")
     
 
-            uninstall_list = []
-            if installed_version == '[]':
-                uninstall_list.append(package_names) # 需要安装新的包
+        #     uninstall_list = []
+        #     if installed_version == '[]':
+        #         uninstall_list.append(package_names) # 需要安装新的包
                 
-            elif final_installed == final_version:
-                print("The " + package_names + " is in the same version,no necessary to install " + package_names + " again")
-            else:
-                uninstall_list.append(package_names) # 需要安装新的包
+        #     elif final_installed == final_version:
+        #         print("The " + package_names + " is in the same version,no necessary to install " + package_names + " again")
+        #     else:
+        #         uninstall_list.append(package_names) # 需要安装新的包
             
-            return uninstall_list
+        #     return uninstall_list
+
