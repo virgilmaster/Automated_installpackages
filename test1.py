885742a5a63dda52c2a373e931956ce16f7e29c7
Virgil <84551549+virgilmaster@users.noreply.github.com>
2022-11-17 12:48:49 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,40 +1,50 @@
+# 文件处理器
 from filehandler import filesdetails
+# 镜像大师
 from sourceholder import mirrors
+# 采集器
 from collector import spiders
 import threading
 import time
 from multiprocessing import Process,Lock
 from queue import Queue
+from inspector import checker
 
-
-# files2 = filesdetails('Windows','requirements.txt')
+# 测试文件处理器模块的功能
+files2 = filesdetails('Windows','requirements.txt')
 # print(files2.counter)
 # print(files2.readinfo)
+# print(files2.filefilter)
 
+import platform
+os = platform.system()
+file = 'requirements.txt'
+files3 = checker(str(os),file)
+print(files3.versioncheck)
 # ali_result = mirrors('aliyun')
 # print(ali_result.mirrorspools)
 
-def runner(y):
-    try:
-        collect_name = spiders(y)
-        collect_name.downloader()
+# def runner(y):
+#     try:
+#         collect_name = spiders(y)
+#         collect_name.downloader()
 
-    except Exception as err:
-        print(err)
+#     except Exception as err:
+#         print(err)
 
-    else:
-        print("Failed to download the resource!!!")
-        time.sleep(5)
+#     else:
+#         print("Failed to download the resource!!!")
+#         time.sleep(5)
 
-if __name__ == '__main__':
-    task_name = ['aliyun','tsinghua','ustc','douban']
-    loop_num = len(task_name)
-    lock = Lock()
-    j = 0
-    while j < loop_num:
-        tk = threading.Thread(target=runner, args=(task_name[j],))
-        #tk.setDaemon(True)
-        tk.start()
-        #tk.join()
-        time.sleep(10)
-        j += 1
+# if __name__ == '__main__':
+#     task_name = ['aliyun','tsinghua','ustc','douban']
+#     loop_num = len(task_name)
+#     lock = Lock()
+#     j = 0
+#     while j < loop_num:
+#         tk = threading.Thread(target=runner, args=(task_name[j],))
+#         #tk.setDaemon(True)
+#         tk.start()
+#         #tk.join()
+#         time.sleep(10)
+#         j += 1
