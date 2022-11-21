018fb3d97107db06ac29be34b41dad0a521cb2d4
Virgil <84551549+virgilmaster@users.noreply.github.com>
2022-11-21 09:48:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # Author: Virgil.She
-# Date: 2022/11/17
-# Version: 0.2.19
+# Date: 2022/11/21
+# Version: 0.2.22
 # Introduction: A fans of python programming language
 
 import os
 import re,string
-import time
-import datetime
+import time,datetime
 import platform
 from filehandler import filesdetails
+from multiprocessing import Process,Lock
+from artist import logwriter
+from progessbar import timebar
+import threading
+from queue import Queue
 
 
 
@@ -23,7 +27,17 @@ def check_system(operation_system):
     print('Your system is: ' + operation_system + '...')
 
 def handle_packages(pack_information):
-    
+    try:
+        # 导入巫师
+        #from beesfly import wizard
+        # 导入检查官
+        from inspector import checker
+    except ImportError as e:
+        raise e
+    # 以下代码进行重构 使用类设计模式 进行全盘重构
+    # 启动 队列 中的线程任务 
+    # 线程的方法正确的应该是在这边进行 导入
+
     final_num = files_read.counter
     numbers = int(final_num)
 
@@ -43,17 +57,11 @@ def handle_packages(pack_information):
             final_installed = (re.sub('[%s]' % re.escape(string.punctuation), '', installed_version)).replace("n","")
             print('{:=^89}'.format("Line"))
 
-            #with open(r'uninstallpack.txt','w') as record:
-            #HashMap = {}
             uninstall = []
             if installed_version == '[]':
                 print(package_names +  " have not be installed")
                 print("Start launch the webspiders,to download the new " + package_names)
                 uninstall.append(package_names)
-
-                #os.system("python download_queue.py")
-                # record = open(r'uninstallpack.txt','w')
-                # record.write(package_names)
             elif final_installed == final_version:
                 print("The " + package_names + " is in the same version,no necessary to install " + package_names + " again")
 
@@ -61,11 +69,8 @@ def handle_packages(pack_information):
                 print("The packages's version is " + installed_version)
                 print("Start to change " + package_names + "'s version,plz wait a moment~.~")
                 uninstall.append(package_names)
-                # record = open(r'uninstallpack.txt','w')
-                # record.write(package_names)
-                # os.system("pip uninstall" + " " + package_names)
-                #os.system("python download_queue.py")
-            print(uninstall)    
+
+                
             
 
         elif operation_system == "Linux":
@@ -77,31 +82,15 @@ def handle_packages(pack_information):
             final_installed = (re.sub('[%s]' % re.escape(string.punctuation), '', installed_version)).replace("n","")
             print('{:=^89}'.format("Line"))
 
-            #with open(r'uninstallpack.txt','w') as record:
             if installed_version == '[]':
                 print(package_names +  " have not be installed")
                 print("Start launch the webspiders,to download the new " + package_names)
-                #os.system("python download_queue.py")
+                
             elif final_installed == final_version:
                 print("The " + package_names + " is in the same version,no necessary to install " + package_names + " again")
             else:
                 print("The packages's version is " + installed_version)
                 print("Start to change" + package_names + "'s version,plz wait a moment~.~")
-                #os.system("pip uninstall" + " " + package_names)
-                #os.system("python download_queue.py")
-
-
-def counter_process(runtime):
-    scale = 100
-    print('{:=^89}'.format("Line"))
-    print("Start downloading the python packages".center(scale // 2, "-"))
-    for i in range(scale + 1):
-        conuter1 = ">" * i
-        counter2 = "-" * (scale - i)
-        counter3 = (i / scale) * 100
-        print("\r{:^3.0f}%[{}>{}]{:.2f}s".format(counter3, conuter1, counter2, runtime), end="")
-        time.sleep(0.1)
-    print("\n" + "All the job is done,lucky so much".center(scale // 2, "-"))
 
 
 if __name__ == "__main__":
@@ -113,17 +102,9 @@ if __name__ == "__main__":
     pack_information = files_read.readinfo
     check_system(operation_system)
     handle_packages(pack_information)
-    try:
-        from artist import logwriter
-        #from beesfly import wizard
-        #from collector import spiders
-    except ImportError as e:
-        print(e)
-    # resource = spiders()
-    # resource.downloader()
     launcher = logwriter(operation_system,pack_information)
     launcher.log_record()
     print("End time is: " + str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
     end_counter = time.perf_counter()
-    runtime = end_counter - start_counter
-    counter_process(runtime)
+    runtime = timebar(start_counter,end_counter)
+    runtime.counter_process()
