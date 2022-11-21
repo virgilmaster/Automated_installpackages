caa3400decdd5a080eb8a8ce84c2984bce0d8eb1
Virgil <84551549+virgilmaster@users.noreply.github.com>
2022-11-21 22:13:14 +0800

    Update collector.py

--- collector.py
+++ collector.py
@@ -1,30 +1,31 @@
 class spiders:
-    def __init__(self,names,packagename):
-        self.names = names
+    def __init__(self,sourcenames,packagename):
+        self.sourcenames = sourcenames
         self.packagename = packagename
 
+
+    @property
     def downloader(self):
         try:
             import os,requests,time,platform
             from jmespath import search
             from sourceholder import mirrors
             from filehandler import filesdetails
-
         except ImportError as e:
             raise e
 
-        os_result = platform.system()
-        file_name = filesdetails(str(os_result),'requirements.txt')
-        final_num = file_name.counter
-        pack_info = file_name.readinfo
+        filesname = 'requirements.txt'
+        require_txt = filesdetails(filesname)
+        final_num = require_txt.counter
+        pack_info = require_txt.readinfo
         numb = int(final_num)
-        final_names = self.names
+        source_names = self.sourcenames
         uninstall_package = self.packagename
 
         for i in range(numb):
             package_detail = str(pack_info[i])
             package_result = package_detail.split("'")[1].split("'")[0]
-            download_pool = mirrors(str(final_names))
+            download_pool = mirrors(str(source_names))
             final_pools = download_pool.mirrorspools
             domain = str(final_pools).split(' ')[1].replace("'", "").replace(",", "")
             link = str(final_pools).split(' ')[3].replace("'", "").replace("}", "").replace("]", "")
@@ -32,16 +33,14 @@ class spiders:
             code_result = resp.status_code
 
             if code_result != 200:
-                print(final_names + ' ' + 'requests error')
-                raise Exception(final_names + ' ' + 'can not download the resources')
+                print(source_names + ' ' + 'requests error')
+                raise Exception(source_names + ' ' + 'can not download the resources')
 
             else:
-                print('Perpare to download the resources!!!')
+                print('Download the resources!!!')
                 time.sleep(5)
-                begin_time = time.time()
                 os.system("pip install " + uninstall_package + " -i " + link + " --trusted-host " + domain)
                 print('{:>^89}'.format(">"))
-                end_time  = time.time()
-                print("The total time is: %s" % (end_time - begin_time))
+
 
 
