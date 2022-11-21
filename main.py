374cc3dd9d0223c59353b9ec95986852f12fef74
Virgil <84551549+virgilmaster@users.noreply.github.com>
2022-11-21 22:48:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Author: Virgil.She
 # Date: 2022/11/21
-# Version: 0.2.3
+# Version: 0.2.5
 # Introduction: A fans of python programming language
 
 import time,datetime
@@ -10,37 +10,39 @@ from artist import logwriter
 from progessbar import timebar
 
 
-def check_system(os_result):
-    print('Dear guests,begin to check your system: ')
-    counter1 = 0
-    while counter1 < 6:
+class runner:
+    def __init__(self,opsys,filename):
+        self.opsys = opsys
+        self.filename = filename
+
+    def check_system(self):
+        os_result = self.opsys
+        print('Dear guests,begin to check your system: ')
+        counter1 = 0
+        while counter1 < 6:
+            time.sleep(1)
+            print('{:=^89}'.format("Checking"))
+            counter1 += 1
         time.sleep(1)
-        print('{:=^89}'.format("Checking"))
-        counter1 += 1
-    time.sleep(1)
-    print('Your system is: ' + os_result + '...')
+        print('Your system is: ' + os_result + '...')
 
 
-def handle_packages():
-    try:
-        from beesfly import wizard
-        from inspector import checker
-        from threading import Lock
-        import threading
-    except ImportError as e:
-        raise e
-    
-    tasklist = ['aliyun', 'tsinghua', 'ustc', 'douban']
-    loop_num = len(tasklist)
-    j = 0
-    while j < loop_num:
-        sourcenames = tasklist[j]
-        witch = wizard(sourcenames)
-        witch.spellmagic
-        j += 1
-        # x =
-        # lock = Lock()
-
+    def handle_packages(self):
+        try:
+            from beesfly import wizard
+            from inspector import checker
+            from threading import Lock
+            import threading
+        except ImportError as e:
+            raise e
+        tasklist = ['aliyun', 'tsinghua', 'ustc', 'douban']
+        loop_num = len(tasklist)
+        j = 0
+        while j < loop_num:
+            sourcenames = tasklist[j]
+            witch = wizard(sourcenames)
+            witch.spellmagic
+            j += 1
 
 if __name__ == "__main__":
     start_counter = time.perf_counter()
@@ -53,9 +55,9 @@ if __name__ == "__main__":
     # lock = Lock()
     # tk = threading.Thread(target=handle_packages, args=(,))
     # tk.start()
-    handle_packages()
-    launcher = logwriter(os_result,pack_information)
+    launcher = logwriter(pack_information)
     launcher.log_record()
+    handle_packages()
     print("End time is: " + str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
     end_counter = time.perf_counter()
     runtime = timebar(start_counter,end_counter)
