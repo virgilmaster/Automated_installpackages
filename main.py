bfd701e96b18e19fd4f61c98c3f107a3865ec1cf
Virgil <84551549+virgilmaster@users.noreply.github.com>
2022-11-21 22:12:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,60 @@
 # Author: Virgil.She
 # Date: 2022/11/21
-# Version: 0.2.28
+# Version: 0.2.3
 # Introduction: A fans of python programming language
 
 import time,datetime
 import platform
 from filehandler import filesdetails
-import threading
 from artist import logwriter
 from progessbar import timebar
-from threading import Lock
 
 
-def check_system(operation_system):
-    print('Dear guests,begin to check your system: ')    
-    counter1 = 0 
+def check_system(os_result):
+    print('Dear guests,begin to check your system: ')
+    counter1 = 0
     while counter1 < 6:
         time.sleep(1)
         print('{:=^89}'.format("Checking"))
         counter1 += 1
     time.sleep(1)
-    print('Your system is: ' + operation_system + '...')
+    print('Your system is: ' + os_result + '...')
 
-def handle_packages(x):
+
+def handle_packages():
     try:
         from beesfly import wizard
         from inspector import checker
         from threading import Lock
+        import threading
     except ImportError as e:
         raise e
-
-    witch = wizard(x)
-    caller = witch.spellmagic
-    lock = Lock()
+    
+    tasklist = ['aliyun', 'tsinghua', 'ustc', 'douban']
+    loop_num = len(tasklist)
+    j = 0
+    while j < loop_num:
+        sourcenames = tasklist[j]
+        witch = wizard(sourcenames)
+        witch.spellmagic
+        j += 1
+        # x =
+        # lock = Lock()
 
 
 if __name__ == "__main__":
     start_counter = time.perf_counter()
-    os_result = platform.system()
-    print("Begin time is: " + str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-    files_read = filesdetails(str(os_result),'requirements.txt')
-    operation_system = platform.system()
-    pack_information = files_read.readinfo
-    check_system(operation_system)
-    tasklist = ['aliyun','tsinghua','ustc','douban']
-    loop_num = len(tasklist)
-    lock = Lock()
-    j = 0
     file = 'requirements.txt'
-    while j < loop_num:
-        tk = threading.Thread(target=handle_packages, args=(tasklist[j],))
-        tk.start()
-        lock.acquire()
-        j += 1
-    launcher = logwriter(operation_system,pack_information)
+    os_result = platform.system()
+    check_system(os_result)
+    print("Begin time is: " + str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    files_read = filesdetails(file)
+    pack_information = files_read.readinfo
+    # lock = Lock()
+    # tk = threading.Thread(target=handle_packages, args=(,))
+    # tk.start()
+    handle_packages()
+    launcher = logwriter(os_result,pack_information)
     launcher.log_record()
     print("End time is: " + str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
     end_counter = time.perf_counter()
