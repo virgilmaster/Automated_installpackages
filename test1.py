addb7fa29efe071e7235d648540334b50572100d
Virgil <84551549+virgilmaster@users.noreply.github.com>
2022-11-21 20:24:13 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -14,28 +14,31 @@ from multiprocessing import Process,Lock
 from queue import Queue
 from inspector import checker
 
-# files2 = filesdetails('Windows','requirements.txt')
-# # print(files2.counter)
-# # print(files2.readinfo)
-# # print(files2.versionfilter)
-# # print(files2.namefilter)
+# test filehandler
+# files2 = filesdetails('requirements.txt')
+# print(type(files2.counter))
+# print(files2.readinfo)
+# print(files2.versionfilter)
+# print(files2.namefilter)
 # namelist = files2.namefilter
 # versionlist = files2.versionfilter
 # print(namelist)
 # print(versionlist)
 
+# test readinfo
+# files = filesdetails('requirements.txt')
+# files33 = files.readinfo
+# for i in range(len(files33)):
+#     #print(files33[i])
+#     #print(type(files33[i]))
+#     print(str(files33[i]).split(',')[0].replace("['",''))
 
-files = filesdetails('Windows','requirements.txt')
-files33 = files.readinfo
-for i in range(len(files33)):
-    #print(files33[i])
-    #print(type(files33[i]))
-    print(str(files33[i]).split(',')[0].replace("['",''))
+# test inspector
 # import platform
 # os = platform.system()
-# file = 'requirements.txt'
-# files3 = checker(str(os),file)
-# print(files3.versioncheck)
+file = 'requirements.txt'
+files3 = checker(file)
+print(files3.versioncheck)
 
 # from beesfly import wizard
 # from inspector import checker
@@ -46,6 +49,13 @@ for i in range(len(files33)):
 # uninstall = task1.versioncheck
 # print(uninstall)
 
-# ali_result = mirrors('aliyun')
-# print(ali_result.mirrorspools)
+
+# test sourceholder
+# tasklist = ['aliyun','tsinghua','ustc','douban']
+# j = 0
+# loop_num = len(tasklist)
+# while j < loop_num:
+#     result = mirrors(tasklist[j])
+#     print(result.mirrorspools)
+#     j += 1
 
