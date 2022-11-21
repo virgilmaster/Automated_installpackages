0ebd488dadccc8710abd15bad438cc2cb85d47ee
Virgil <84551549+virgilmaster@users.noreply.github.com>
2022-11-21 22:15:42 +0800

    Update beesfly.py

--- beesfly.py
+++ beesfly.py
@@ -1,31 +1,28 @@
-import time
-import threading
-from threading import Lock
-from collector import spiders
-
-
 class wizard():
     def __init__(self,sourcename):
         self.sourcename = sourcename
 
+    @property
     def spellmagic(self):
         try:
             from collector import spiders
             from inspector import checker
-            import os,platform
+            from threading import Lock
+            from multiprocessing import Lock as Lk
+            import os,platform,time
         except ImportError as error:
             raise error
         lock = Lock()
         source_name = self.sourcename
         file_name = 'requirements.txt'
-        os_result = platform.system()
-        checker_pack = checker(os_result,file_name)
+        checker_pack = checker(file_name)
         uninstall_pack = checker_pack.versioncheck
         try:
             for j in range(len(uninstall_pack)):
+                lock.acquire()
                 final_uninstall = uninstall_pack[j]
                 butterfly = spiders(source_name,final_uninstall)
-                butterfly.downloader()
+                butterfly.downloader
                 lock.release()
         except Exception as err:
             raise err
