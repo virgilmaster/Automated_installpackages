9035500150dc2c3cdcb02c1d1560342d88618461
Virgil <84551549+virgilmaster@users.noreply.github.com>
2022-11-21 13:54:54 +0800

    Update inspector.py

--- inspector.py
+++ inspector.py
@@ -26,27 +26,28 @@ class checker:
         numbers = int(final_num)
         if os_result == 'Windows':
             uninstall_list = []
-            for x in range(numbers):
-                packages_installed = os.popen('pip list | findstr' + ' ' + namelist[x]) 
+            z = 0
+            while z < numbers:
+            #for x in range(numbers):
+                packages_installed = os.popen('pip list | findstr' + ' ' + namelist[z]) 
                 result_installed = packages_installed.readlines()     
                 packages_installed.close()                              
                 installed_version = str(result_installed).split(" ")[-1]
                 final_installed = (re.sub('[%s]' % re.escape(string.punctuation), '', installed_version)).replace("n","")
-                package_names = namelist[x]
-                final_version = versionlist[x]
+                package_names = namelist[z]
+                final_version = versionlist[z]
+                
                 '''   
                 Plz God bless me,let it run.
                 '''
-                if final_installed == '[]':
-                    print('The' + package_names + 'is not installed on the computer')
-                    uninstall_list.append(package_names) 
+                if final_installed != final_version:
+                    print('Prepare to download the' + ' ' + package_names)
+                    uninstall_list.append(package_names)
+                    print('{:>^89}'.format(">")) 
                 elif final_installed == final_version:
                     print(package_names,'have no necessary to install')
-                else:
-                    print('The' + package_names + 'is not correct version on the computer')
-                    os.system('pip uninstall' + package_names)
-                    uninstall_list.append(package_names)
-
+                    print('{:>^89}'.format(">"))
+                z += 1
             return uninstall_list 
 
 
@@ -63,14 +64,12 @@ class checker:
                 '''   
                 Plz bless me,let it run.
                 '''
-                if final_installed == '[]':
-                    print('The' + package_names + 'is not installed on the computer')
-                    uninstall_list.append(package_names) 
+                if final_installed != final_version:
+                    print('Prepare to download the' + ' ' + package_names)
+                    uninstall_list.append(package_names)
+                    print('{:>^89}'.format(">")) 
                 elif final_installed == final_version:
                     print(package_names,'have no necessary to install')
-                else:
-                    print('The' + package_names + 'is not correct version on the computer')
-                    os.system('pip uninstall' + package_names)
-                    uninstall_list.append(package_names)
+                    print('{:>^89}'.format(">"))
 
             return uninstall_list 
