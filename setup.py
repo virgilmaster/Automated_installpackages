73409815b1d11e59b06fd7ff39d3689c5c04e1ae
Virgil <84551549+virgilmaster@users.noreply.github.com>
2022-11-22 10:52:28 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="Automated_installpackages",
+    version="1.0",
+    author="Vergil",
+    author_email="691167837@qq.com",
+    description=("This is a automation project"),
+    keywords="automation",
+    url="https://github.com/virgilmaster/Automated_installpackages",
+    packages=["Automated_installpackages","Automated_installpackages.Db"],
+)
+
