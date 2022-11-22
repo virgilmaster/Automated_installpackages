4aaeef7df85bd734f0640e53cb3ffe30c7ab360e
Virgil <84551549+virgilmaster@users.noreply.github.com>
2022-11-22 11:38:53 +0800

    Update beesfly.py

--- beesfly.py
+++ beesfly.py
@@ -10,22 +10,31 @@ class wizard():
             from threading import Lock
             from multiprocessing import Lock as Lk
             import os,platform,time
+
         except ImportError as error:
             raise error
+
         lock = Lock()
         source_name = self.sourcename
         file_name = 'requirements.txt'
         checker_pack = checker(file_name)
         uninstall_pack = checker_pack.versioncheck
+
         try:
+            # Try to loop out the packages that are not installed and send them to the instanced butterfly for downloading
             for j in range(len(uninstall_pack)):
                 lock.acquire()
                 final_uninstall = uninstall_pack[j]
-                butterfly = spiders(source_name,final_uninstall)
-                butterfly.downloader
-                lock.release()
+
+            #Instantiate butterfly downloader
+            #Multithread Start
+            butterfly = spiders(source_name,final_uninstall)
+            butterfly.downloader
+            lock.release()
+
         except Exception as err:
             raise err
+
         else:
             print('Resources download fail',source_name,'have some problem')
             time.sleep(1)
